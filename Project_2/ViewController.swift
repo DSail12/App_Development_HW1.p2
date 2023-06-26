532d75740d22f581d7dee16c6c128ddfdad8c617
dsail <dsail@yandex.ru>
2023-06-27 01:07:04 +0300

    Initial Commit

---
+++ Project_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_2
+//
+//  Created by Eugene Zvyagin on 25.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var authorization: UILabel!
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func Button(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
